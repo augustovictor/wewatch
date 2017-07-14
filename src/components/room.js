0fe657ee992311b4810486d54a66fb99ed937ff7
Victor <victoraweb@gmail.com>
2017-07-14 11:30:57 -0300

    [REFACTOR] Moved input search to room file

--- src/components/room.js
+++ src/components/room.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
-import SearchInput from './search_input';
+import { connect } from 'react-redux';
+import { changeUrl } from '../actions';
+import Youtube from 'react-youtube';
 
 class Room extends Component {
+    onSearchInputChange(event) {
+        const value = event.target.value;
+        const videoId = value.substr(value.indexOf('=') +1, value.length);
+        //const videoId = value;
+        this.props.changeUrl(videoId);
+    }
+
+    onPlay() {
+        console.log('Video running...');
+    }
+
+    onPause() {
+        console.log('Video paused.')
+    }
+
     render() {
         return(
             <div>
-                <h1>ROOM</h1>
-                <SearchInput />
+                <h1>Room</h1>
+                <input type="text" placeholder="Enter the youtube url" onChange={ this.onSearchInputChange.bind(this) } />
+                <Youtube
+                    videoId={ this.props.videoId }
+                    onPlay={ this.onPlay }
+                    onPause={ this.onPause }
+                />
             </div>
         );
     }
 }
 
-export default Room;
+function mapStateToProps({ videoId }) {
+    return { videoId }
+}
+
+export default connect(mapStateToProps, { changeUrl })(Room);
